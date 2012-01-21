5acc1506bc5a2ca273f18fd2574e9018b7f93619
Derrick Weis <derrick@derrickweis.com>
2012-01-21 00:08:27 -0500

    Add randomly coloured boxes to the screen when you click a button

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,25 @@
 var App = Em.Application.create();
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
+App.Box = Em.Object.extend({
+  color: null
+});
+
+App.boxController = Em.ArrayProxy.create({
+  content: [],
+
+  createBox: function() {
+    var random = Math.floor(Math.random() * 3)
+
+    var color = null;
+
+    switch (random) {
+      case 1: color = 'green'; break;
+      case 2: color = 'blue'; break
+      default: color = 'red'; break;
+    }
+
+    var box = App.Box.create({ color: color });
+
+    this.pushObject(box);
   }
 });
